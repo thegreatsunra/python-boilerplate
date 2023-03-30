4a76c1b0833f4b783d2ca3447f4fb1ae9cb94c04
Dane Petersen <thegreatsunra@gmail.com>
2023-03-29 20:41:04 -0500

    Reformat via poetry run black

--- src/main.py
+++ src/main.py
@@ -3,13 +3,16 @@
 Main module docstring
 """
 
+
 def hello_world():
     """Returns 'Hello World'"""
     return "Hello World"
 
+
 def main():
-    """ Main entry point of the app """
+    """Main entry point of the app"""
     print(hello_world())
+
 
 if __name__ == "__main__":
     ## This is executed when run from the command line
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -5,6 +5,7 @@ Tests
 
 from src.main import hello_world
 
+
 def test_main():
     """Test main module"""
     assert hello_world() == "Hello World"
