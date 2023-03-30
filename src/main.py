dc201031caa099d0c5cb11f7320bdc3136d173c8
Dane Petersen <thegreatsunra@gmail.com>
2023-03-29 19:51:44 -0500

    Refactor into main module, default execution context, hello_world function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
-
 """
-Prints Hello World
+Main module docstring
 """
 
-print("Hello World")
+def hello_world():
+    """Returns 'Hello World'"""
+    return "Hello World"
+
+def main():
+    """ Main entry point of the app """
+    print(hello_world())
+
+if __name__ == "__main__":
+    ## This is executed when run from the command line
+    main()
